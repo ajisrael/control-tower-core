daa8782cabf339818f9d53e3ffaea2d7494ac628
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-07 20:00:16 -0400

    f   - added throwErrorIfEntityDoesNotExist() helper method

--- src/main/java/control/tower/core/utils/Helper.java
+++ src/main/java/control/tower/core/utils/Helper.java
@@ -1,7 +1,14 @@
 package control.tower.core.utils;
 
 public class Helper {
+
     public static boolean isNullOrBlank(String string) {
         return string == null || string.isBlank();
+    }
+
+    public static void throwErrorIfEntityDoesNotExist(Object entity, String message) {
+        if (entity == null) {
+            throw new IllegalStateException(message);
+        }
     }
 }
