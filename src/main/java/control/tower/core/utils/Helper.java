cc36be6da01cc677e98e0ec06d95d1b2d32dabf6
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-09 05:55:57 -0400

    r   - moved hash fn from address service

--- src/main/java/control/tower/core/utils/Helper.java
+++ src/main/java/control/tower/core/utils/Helper.java
@@ -1,5 +1,9 @@
 package control.tower.core.utils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Helper {
 
     public static boolean isNullOrBlank(String string) {
@@ -9,6 +13,27 @@ public class Helper {
     public static void throwErrorIfEntityDoesNotExist(Object entity, String message) {
         if (entity == null) {
             throw new IllegalStateException(message);
+        }
+    }
+
+    public static String calculateSHA256Hash(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Handle the exception accordingly
+            e.printStackTrace();
+            return null;
         }
     }
 }
