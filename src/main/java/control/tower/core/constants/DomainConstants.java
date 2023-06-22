e24d185a46ce840ee9747c99a03c76e4400650ef
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-06-22 17:46:43 -0400

    f   - added api gateway url

---
+++ src/main/java/control/tower/core/constants/DomainConstants.java
@@ -0,0 +1,10 @@
+package control.tower.core.constants;
+
+public class DomainConstants {
+
+    private DomainConstants() {
+        throw new IllegalStateException("Constants class");
+    }
+
+    public static final String API_GATEWAY_URL = "http://localhost:8082/";
+}
